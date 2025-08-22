6512f3a38be158635fe26ed08226cd2af997b914
Punit9464 <punitkumar8728@gmail.com>
2025-08-22 22:58:19 +0530

    exp 3 sql

--- EXPERIMENT 3/Experiment 3.sql
+++ EXPERIMENT 3/Experiment 3.sql
@@ -229,6 +229,61 @@ select name, id from b;
 */
 
 
+-- Medium
+CREATE TABLE department (
+    id INT PRIMARY KEY,
+    dept_name VARCHAR(50)
+);
+
+-- Create Employee Table
+CREATE TABLE employee (
+    id INT,
+    name VARCHAR(50),
+    salary INT,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+
+-- Insert into Department Table
+INSERT INTO department (id, dept_name) VALUES
+(1, 'IT'),
+(2, 'SALES');
+
+-- Insert into Employee Table
+INSERT INTO employee (id, name, salary, department_id) VALUES
+(1, 'JOE', 70000, 1),
+(2, 'JIM', 90000, 1),
+(3, 'HENRY', 80000, 2),
+(4, 'SAM', 60000, 2),
+(5, 'MAX', 90000, 1);
+
+
+Select d.dept_name, e.name, e.salary
+from
+employee as e
+inner join 
+department d
+on e.department_id = d.id
+where e.salary in 
+(select max(salary) from
+employee group by department_id);
+
+-- or
+
+
+Select e.name, d.dept_name, e.salary 
+from
+employee as e
+inner join 
+department d
+on e.department_id = d.id
+where e.salary in 
+(Select max(salary) from 
+employee where department_id = e.department_id)
+order by d.dept_name;
+
+
 -- HARD: GIVEN TWO TABLES OUTPUT THEM AS FOLLOWS:
 
 CREATE TABLE TBL_A (
